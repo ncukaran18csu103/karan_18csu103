54f2dbd2b4dd9b5e7aba47951ef48efd7fb5bc24
karan choudhary <karan18csu103@ncuindia.edu>
2019-01-11 11:44:17 +0530

    adding programs

---
+++ FOCPII/datatypes/src/demoint.java
@@ -0,0 +1,14 @@
+import java.util.*
+public class ScannerMultiplyNumbers
+{
+
+public static voidmain(String[] args)
+{
+Scanner ConsoleInput=new Scanner(System.in);
+System.out.print("Please type two numbeers:");
+int num1=consoleInpu.nextInt();
+int num2=consoleInpu.nextInt();
+int prod= num1*num2;
+System.out.println("the product is"+prod);
+ }
+}
