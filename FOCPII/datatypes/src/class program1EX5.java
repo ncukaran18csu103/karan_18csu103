cc3d45380f43b80812afcfc48e4d7736a487eb63
ncukaran18csu103 <46473542+ncukaran18csu103@users.noreply.github.com>
2019-01-22 16:45:31 +0530

    Add files via upload

---
+++ FOCPII/datatypes/src/class program1EX5.java
@@ -0,0 +1,18 @@
+class program1EX5
+{
+public static void main(String[] args)
+{
+int[]arr=new int[10]
+System.out.println("enter elements of arrays");
+Scanner scan=new Scanner(System.in);
+int a;
+for(a=1;a<10;a++)
+ {
+System.out.println("enter value");
+a=scan.nextint();
+}
+System.out.println("enter values to be searched");
+int searchval=scn.nextInt();
+
+}
+}
